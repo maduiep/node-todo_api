db9f8a2f802ac30087828753ead39580fbe6da99
maduiep <emmanuelmaduip@gmail.com>
2020-05-23 12:55:31 +0100

    adding public and views folder with added files

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const todoRoutes = require('./routes/todo')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views'))
 
-app.get('/', (req, res) => res.send('Hello from root routes'))
+
+app.get('/', (req, res) => res.sendFile('index.html'))
 app.use('/api/todos', todoRoutes)
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
