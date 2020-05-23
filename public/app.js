c6b2e7abf4f83fc43f018dc0c12ab711c212bfef
maduiep <emmanuelmaduip@gmail.com>
2020-05-23 18:17:13 +0100

    connecting the form to our api using input field

--- public/app.js
+++ public/app.js
@@ -1,12 +1,38 @@
 $(document).ready(function () {
  $.getJSON('/api/todos')
   .then(addTodos)
+
+ $('#todoInput').keypress(function (event) {
+  if (event.which === 13) {
+   createTodo()
+  }
+ })
 })
 
 function addTodos(todos) {
  // add todos to the page here
  todos.forEach((todo) => {
-  var newTodo = $('<li class="task">' + todo.name +'</li>')
-  $('.list').append(newTodo)
+  addTodo(todo)
  })
+}
+
+function addTodo(todo) {
+ var newTodo = $('<li class="task">' + todo.name + '</li>')
+ if (todo.completed) {
+  newTodo.addClass('done')
+ }
+ $('.list').append(newTodo)
+}
+
+function createTodo() {
+ // send a post request to create new todo
+ var userInput = $('#todoInput').val()
+ $.post('/api/todos', { name: userInput })
+  .then((newTodo) => {
+   $('#todoInput').val('')
+   addTodo(newTodo)
+  })
+  .catch((err) => {
+   console.log(err)
+  })
 }
