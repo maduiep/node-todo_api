aaad9e88da6d0893b0a07530f8fb0d7a65d2b293
maduiep <emmanuelmaduip@gmail.com>
2020-05-23 09:34:15 +0100

    installed body-parser and created a todo route

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser')
 const todoRoutes = require('./routes/todo')
 
-app.use('/api/todos', todoRoutes)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.send('Hello from root routes'))
+app.use('/api/todos', todoRoutes)
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
--- routes/todo.js
+++ routes/todo.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const router = express.Router()
+
 const db = require('../models')
 
-router.get('/', (req, res) => res.send(db.Todo.find()
- .then((todos) => {
-  res.json(todos)
- })
- .catch((err) => {
-  res.send(err)
- })
-))
+router.get('/', (req, res) => {
+  db.Todo.find()
+    .then((todos) => {
+      res.json(todos)
+    })
+    .catch((err) => {
+      res.send(err)
+    })
+})
+
+router.post('/', (req, res) => {
+  db.Todo.create(req.body)
+    .then((newTodo) => {
+      res.status(201).json(newTodo)
+    })
+    .catch((err) => {
+      res.send(err)
+    })
+})
 
 module.exports = router
