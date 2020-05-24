456793a6d401a52b2250949fa2a7d2fdb4667d46
maduiep <emmanuelmaduip@gmail.com>
2020-05-24 14:28:21 +0100

    adding a toogleClass to our todolist may be final commit(:

--- public/app.js
+++ public/app.js
@@ -8,11 +8,12 @@ $(document).ready(function () {
     }
   })
 
-  // $('.list').on('click', 'li', function () {
-  //   alert('clicked')
-  // })
+  $('.list').on('click', 'li', function () {
+    updateTodo($(this))
+  })
 
-  $('.list').on('click', 'span', function () {
+  $('.list').on('click', 'span', function (e) {
+    e.stopPropagation()
     removeTodo($(this).parent())
   })
 })
@@ -27,6 +28,7 @@ function addTodos(todos) {
 function addTodo(todo) {
   var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>')
   newTodo.data('id', todo._id)
+  newTodo.data('completed', todo.completed)
   if (todo.completed) {
     newTodo.addClass('done')
   }
@@ -59,4 +61,22 @@ function removeTodo(todo) {
     .catch((err) => {
       console.log(err)
     })
+}
+
+function updateTodo(todo) {
+  var updateUrl = '/api/todos/' + todo.data('id')
+  var isDone = !todo.data('completed')
+  var updateData = { completed: isDone }
+  // console.log(updateData)
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+    .then((updatedTodo) => {
+      todo.toggleClass('done')
+      todo.data('completed', isDone)
+      // console.log(updatedTodo)
+    })
+  //console.log(todo.data('completed'))
 }
